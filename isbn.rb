67be29bd3b16b210922120966f8613031516709e
Brian Lewis <blewis8958@gmail.com>
2017-02-03 13:24:28 -0500

    Played around in class added some more test cases and found out I need to look into invalid character entry

--- isbn.rb
+++ isbn.rb
@@ -16,11 +16,28 @@ def isbn_space_deleter(isbn_num)
 	return new_num
 end
 
+# def isbn_invalid_characters_input(isbn_num)
+# 	if (isbn_num.class != String)
+# 		return "Error entry must be a string"
+# 	else
+# 		isbn_array = isbn_num.split("")
+
+# 		(isbn_array.length).times do |x|
+# 			if (isbn_array[x].to_i != Fixnum)
+# 				return "Error, Entry must be a String of Fixnums"
+# 			end
+# 		end
+# 	end
+		
+# end
+
 def isbn_10_sumcheck(isbn_num)
 
 	isbn_10_checker(isbn_num)
 	isbn_dash_deleter(isbn_num)
 	isbn_space_deleter(isbn_num)
+	
+	# error_holder = isbn_invalid_characters_input(isbn_num)
 
 	isbn_array = isbn_num.split("")
 	array_length = isbn_array.length
@@ -50,6 +67,8 @@ def isbn_10_sumcheck(isbn_num)
 
 	if checksum == isbn_number_array.last
 		return true
+	# elsif (error_holder == "Error entry must be a string") || (error_holder == "Error, Entry must be a String of Fixnums")
+	# 	return error_holder
 	else
 		return false
 	end
@@ -90,19 +109,19 @@ def isbn_13_sumcheck(isbn_num13)
 		if (i % 2 == 1)
 			sum += 3 * isbn_number_array[i]
 
-			print isbn_number_array[i], " "
-			puts sum
+			# print isbn_number_array[i], " "
+			# puts sum
 			
 		else
 			sum += 1 * isbn_number_array[i]
 
-			print isbn_number_array[i], " "
-			puts sum
+			# print isbn_number_array[i], " "
+			# puts sum
 		end
 		#puts isbn_number_array[i - 1]
 	end
 
-	print sum
+	# print sum
 
 	checksum = sum % 10
 
--- test_isbn.rb
+++ test_isbn.rb
@@ -3,33 +3,33 @@ require_relative "isbn.rb"
 
 class TestISBN < Minitest::Test
 
-	# def test_length_is_equal_to_10
-	# 	assert_equal(true,isbn_10_checker("123456789x"))
-	# end
+	def test_length_is_equal_to_10
+		assert_equal(true,isbn_10_checker("123456789x"))
+	end
 
-	# def test_length_is_less_than_10
-	# 	assert_equal(false,isbn_10_checker("12345"))
-	# end
+	def test_length_is_less_than_10
+		assert_equal(false,isbn_10_checker("12345"))
+	end
 
-	# def test_length_is_greater_than_10
-	# 	assert_equal(false,isbn_10_checker("112344567899"))
-	# end
+	def test_length_is_greater_than_10
+		assert_equal(false,isbn_10_checker("112344567899"))
+	end
 
-	# def test_gets_rid_of_dashes
-	# 	assert_equal("12345", isbn_dash_deleter("1-2-3-4-5"))
-	# end
+	def test_gets_rid_of_dashes
+		assert_equal("12345", isbn_dash_deleter("1-2-3-4-5"))
+	end
 
-	# def test_gets_rid_of_spaces
-	# 	assert_equal("12345", isbn_space_deleter("1 2 3 4 5"))
-	# end
+	def test_gets_rid_of_spaces
+		assert_equal("12345", isbn_space_deleter("1 2 3 4 5"))
+	end
 
-	# def test_successful_case_isbn10
-	# 	assert_equal(true, isbn_10_sumcheck("0471958697"))
-	# end
+	def test_successful_case_isbn10
+		assert_equal(true, isbn_10_sumcheck("0471958697"))
+	end
 
-	# def test_failed_case_isbn10
-	# 	assert_equal(false,isbn_10_sumcheck("4780470059029"))
-	# end
+	def test_failed_case_isbn10
+		assert_equal(false,isbn_10_sumcheck("4780470059029"))
+	end
 
 
 
@@ -52,5 +52,9 @@ class TestISBN < Minitest::Test
 	def test_failed_case_isbn13
 		assert_equal(false,isbn_13_sumcheck("4780470059029"))
 	end
+
+	# def test_invalid_character_at_symbol_isbn10
+	# 	assert_equal("Error, Entry must be a String of Fixnums",isbn_10_sumcheck("0-321@14653-0"))
+	# end
 
 end
